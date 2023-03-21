ea81f90f7ae70216519780b23c778ccfb2060e42
Lavra Tamutus <lavratamutus@gmail.com>
2023-03-21 13:27:37 -0400

    Initialize

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>We did it!</h1>
+        <h2>Congratulations, there's no more money</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+body {
+    background-color: red;
+}
+</style>
